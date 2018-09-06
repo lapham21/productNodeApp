bd3f100895e34bb1e45b9eda0c93c7761ac5bebc
Nolan Lapham <>
2018-09-05 22:02:14 -0400

    master: finished up api for interaction with the database

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -7,11 +7,29 @@ exports.product_create = function (req, res) {
             price: req.body.price
         }
     );
-
     product.save(function (err) {
-        if (err) {
-            return next(err);
-        }
+        if (err) return next(err);
         res.send('Product Created successfully')
+    })
+};
+
+exports.product_details = function (req, res) {
+    Product.findById(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    })
+};
+
+exports.product_update = function (req, res) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+        if (err) return next(err);
+        res.send('Product updated.');
+    })
+};
+
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Product deleted.');
     })
 };
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
 const productController = require('../controllers/product.controller');
 
 router.post('/create', productController.product_create);
+router.get('/:id', productController.product_details);
+router.put('/:id/update', productController.product_update);
+router.delete('/:id/delete', productController.product_delete);
 
 module.exports = router;
